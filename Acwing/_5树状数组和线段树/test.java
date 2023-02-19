fa595091bc4c9af5a88afae3d223541c52cb404f
tyy13588 <83629778+tyy13588@users.noreply.github.com>
2023-02-19 15:21:25 +0800

    Revert "hot-fix commit2"
    This reverts commit 70afe5faebd1473492c3ae9642744f3cd852f3ca.

--- Acwing/_5树状数组和线段树/test.java
+++ Acwing/_5树状数组和线段树/test.java
@@ -3,6 +3,5 @@ package Acwing._5树状数组和线段树;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello hot-fix 2");
-        System.out.println("hello master2");
     }
 }
